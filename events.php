befb0b82cf9ef8922143e7d922a3e608b401e1f3
Boroma <boromamwale@gmail.com>
2016-12-20 05:01:25 +0200

    added an events.php (draft) with some images will do clean up next comit..
    and links are working to dummy events

---
+++ events.php
@@ -0,0 +1,255 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 19/12/2016
+ * Time: 02:18
+ */
+?>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" />
+    <link rel="stylesheet" href="css/animate.css" type="text/css" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" type="text/css" />
+    <link rel="stylesheet" href="css/simple-line-icons.css" type="text/css" />
+    <link rel="stylesheet" href="css/font.css" type="text/css" />
+    <link rel="stylesheet" href="css/app.css" type="text/css" />
+    <!--[if lt IE 9]>
+    <script src="js/ie/html5shiv.js"></script>
+    <script src="js/ie/respond.min.js"></script>
+    <script src="js/ie/excanvas.js"></script>
+    <![endif]-->
+</head>
+<body class="">
+<section class="vbox">
+    <section>
+        <section class="hbox stretch">
+            <section id="content">
+                <section class="hbox stretch">
+                    <!-- .aside -->
+                    <aside>
+                        <section class="vbox">
+                            <section class="scrollable wrapper">
+                                <div class="timeline">
+                                    <article class="timeline-item active">
+                                        <div class="timeline-caption">
+                                            <div class="panel bg-primary lt no-borders">
+                                                <div class="panel-body">
+                                                    <h4 class="m-t-none">Event Timeline</h4>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article class="timeline-item">
+                                        <div class="timeline-caption">
+                                            <div class="panel panel-default">
+                                                <div class="panel-body">
+                                                    <span class="arrow left"></span>
+                                                    <span class="timeline-icon"><i class="fa fa-code time-icon bg-dark"></i></span>
+                                                    <span class="timeline-date">15th Jan 2017</span>
+
+                                                    <!-- sliding event card -->
+                                                    <div class="carousel slide auto" data-interval="6000">
+                                                        <div class="carousel-inner">
+                                                            <div class="item active">
+                                                                <div class="item-overlay opacity animated fadeInDown wrapper bg-info">
+                                                                    <p class="text-white">What you waiting for??</p>
+                                                                    <div><a href="https://www.eventbrite.com/e/test-event-tickets-30444529407?ref=elink" target="_blank"><i class="fa fa-plus icon-info inline-blockx"></i></a></div>
+                                                                    <a href="http://www.eventbrite.com/e/test-event-tickets-30444529407?ref=ebtnebtckt" target="_blank">
+                                                                        <img src="https://www.eventbrite.com/custombutton?eid=30444529407" alt="Eventbrite - Test Event" />
+                                                                    </a>
+                                                                    </div>
+                                                                <div class="bottom wrapper bg-info gd">
+                                                                    <div class="m-t m-b"><a href="#" class="b-b b-danger h2 text-u-c text-lt font-bold">Test Event</a></div>
+                                                                    <p class="hidden-xs">Epic coding things going on here!!.. nothing less tha extra ordinary..  </p>
+                                                                </div>
+                                                                <a href="#"><img src="images/testev1.png" alt="" class="img-full"></a>
+                                                            </div>
+                                                            <div class="item">
+                                                                <div class="item-overlay opacity animated fadeInDown wrapper bg-info">
+                                                                    <div><a href="https://www.eventbrite.com/e/test-event-tickets-30444529407?ref=elink" target="_blank"><i class="fa fa-plus icon-info inline-blockx"></i></a></div>
+                                                                    <p class="text-white">Sign Up here!!!...</p>
+                                                                    <a href="https://www.eventbrite.com/e/test-two-tickets-30446622668?ref=elink" target="_blank" style="color:#005580">Click for more info..</a>
+                                                                    <a href="http://www.eventbrite.com/e/test-event-tickets-30444529407?ref=ebtnebtckt" target="_blank">
+                                                                        <img src="https://www.eventbrite.com/custombutton?eid=30444529407" alt="Eventbrite - Test Event" />
+                                                                    </a>
+                                                                </div>
+                                                                <div class="bottom wrapper bg-info gd">
+                                                                    <div class="m-t m-b"><a href="#" class="b-b b-warning h2 text-u-c text-lt font-bold">Test Event</a></div>
+                                                                    <p class="hidden-xs">Time to get css on the road!!... Best believe...</p>
+                                                                </div>
+                                                                <a href="#"><img src="images/testev2.png" alt="" class="img-full"></a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- /sliding event card -->
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article class="timeline-item alt">
+                                        <div class="timeline-caption">
+                                            <div class="panel panel-default">
+                                                <div class="panel-body">
+                                                    <span class="arrow right"></span>
+                                                    <span class="timeline-icon"><i class="fa fa-male time-icon bg-success"></i></span>
+                                                    <span class="timeline-date">21st May 2017</span>
+
+                                                    <!-- sliding event card 2-->
+                                                    <div class="carousel slide auto" data-interval="9000">
+                                                        <div class="carousel-inner">
+                                                            <div class="item active">
+                                                                <div class="item-overlay opacity animated fadeInDown wrapper bg-info">
+                                                                    <p class="text-white">What you waiting for??</p>
+                                                                    <div><a href="https://www.eventbrite.com/e/test-two-tickets-30446622668?ref=elink" target="_blank"><i class="fa fa-plus icon-info inline-blockx"></i></a></div>
+                                                                    <a href="http://www.eventbrite.com/e/test-event-tickets-30444529407?ref=ebtnebtckt" target="_blank">
+                                                                        <img src="https://www.eventbrite.com/custombutton?eid=30444529407" alt="Eventbrite - Test Event" />
+                                                                    </a>
+                                                                </div>
+                                                                <div class="bottom wrapper bg-info gd">
+                                                                    <div class="m-t m-b"><a href="#" class="b-b b-danger h2 text-u-c text-lt font-bold">Test Event 2</a></div>
+                                                                    <p class="hidden-xs">css is my son!!!</p>
+                                                                </div>
+                                                                <a href="#"><img src="images/testev3.png" alt="" class="img-full"></a>
+                                                            </div>
+                                                            <div class="item">
+                                                                <div class="item-overlay opacity animated fadeInDown wrapper bg-info">
+                                                                    <p class="text-white">Gaming say what!!!</p>
+                                                                    <div><a href="https://www.eventbrite.com/e/test-two-tickets-30446622668?ref=elink" target="_blank"><i class="fa fa-plus icon-info inline-blockx"></i></a></div>
+                                                                    <a href="http://www.eventbrite.com/e/test-event-tickets-30444529407?ref=ebtnebtckt" target="_blank">
+                                                                        <img src="https://www.eventbrite.com/custombutton?eid=30444529407" alt="Eventbrite - Test Event" />
+                                                                    </a>
+                                                                </div>
+                                                                <div class="bottom wrapper bg-info gd">
+                                                                    <div class="m-t m-b"><a href="#" class="b-b b-warning h2 text-u-c text-lt font-bold">Test Event 2</a></div>
+                                                                    <p class="hidden-xs">Yeah css got nothing on me..</p>
+                                                                </div>
+                                                                <a href="#"><img src="images/testev4.png" alt="" class="img-full"></a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- /sliding event card 2-->
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <article class="timeline-item">
+                                        <div class="timeline-caption">
+                                            <div class="panel panel-default">
+                                                <div class="panel-body">
+                                                    <span class="arrow left"></span>
+                                                    <span class="timeline-icon"><i class="fa fa-plane time-icon bg-dark"></i></span>
+                                                    <span class="timeline-date">8th July 2017</span>
+
+                                                    <!-- sliding event card 3 -->
+                                                    <div class="carousel slide auto" data-interval="3000">
+                                                        <div class="carousel-inner">
+                                                            <div class="item active">
+                                                                <div class="item-overlay opacity animated fadeInDown wrapper bg-info">
+                                                                    <p class="text-white">Warframe!!!!</p>
+                                                                    <a href="http://www.eventbrite.com/e/test-event-tickets-30444529407?ref=ebtnebtckt" target="_blank">
+                                                                        <img src="https://www.eventbrite.com/custombutton?eid=30444529407" alt="Eventbrite - Test Event" />
+                                                                    </a>
+                                                                </div>
+                                                                <div class="bottom wrapper bg-info gd">
+                                                                    <div class="m-t m-b"><a href="#" class="b-b b-danger h2 text-u-c text-lt font-bold">Test Event 3</a></div>
+                                                                    <p class="hidden-xs">Unstoppable</p>
+                                                                </div>
+                                                                <a href="#"><img src="images/warframe_rhino_wallaper_by_jithvan-d804kz9.png" alt="" class="img-full"></a>
+                                                            </div>
+                                                            <div class="item">
+                                                                <div class="item-overlay opacity animated fadeInDown wrapper bg-info">
+                                                                    <p class="text-white">Gaming say what!!!</p>
+                                                                    <a href="http://www.eventbrite.com/e/test-event-tickets-30444529407?ref=ebtnebtckt" target="_blank">
+                                                                        <img src="https://www.eventbrite.com/custombutton?eid=30444529407" alt="Eventbrite - Test Event" />
+                                                                    </a>
+                                                                </div>
+                                                                <div class="bottom wrapper bg-info gd">
+                                                                    <div class="m-t m-b"><a href="#" class="b-b b-warning h2 text-u-c text-lt font-bold">Test Event 3</a></div>
+                                                                    <p class="hidden-xs">Beast!!</p>
+                                                                </div>
+                                                                <a href="#"><img src="images/warframe_rhino_wallaper_by_jithvan-d804kz9.png" alt="" class="img-full"></a>
+                                                            </div>
+                                                        </div>
+                                                    </div>
+                                                    <!-- /sliding event card 3 -->
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </article>
+                                    <div class="timeline-footer"><a href="#"><i class="fa fa-plus time-icon inline-block bg-dark"></i></a></div>
+                                </div>
+                            </section>
+                        </section>
+                    </aside>
+                    <!-- /.aside -->
+                    <!-- .aside -->
+                    <aside class="aside-lg bg-light b-r" id="aside">
+                        <div class="wrapper">
+                            <h4 class="m-t-none">Timeline Key</h4>
+                            <div class="post-sum">
+                                <p>Typing in here to see how well the content will look. Too tired to think of something smart but i guess thiss will have to do.
+                                    <br><br>
+                                    This should have content talking about how awesome our events are and all as well as explaining the time line key..
+                                    <br><br>
+                                    Its pretty random I know but bare with me. Illustration purposes i promise to clean it up in next comit.
+                                </p>
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <i class="icon-user-female"></i>
+                                icon-user-female
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <i class="icon-users"></i>
+                                icon-users
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <i class="icon-user-follow"></i>
+                                icon-user-follow
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <i class="icon-user-following"></i>
+                                icon-user-following
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <i class="icon-user-unfollow"></i>
+                                icon-user-unfollow
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <i class="icon-trophy"></i>
+                                icon-trophy
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <i class="icon-speedometer"></i>
+                                icon-speedometer
+                            </div>
+                            <div class="col-md-3 col-sm-4">
+                                <i class="icon-social-youtube"></i>
+                                icon-social-youtube
+                            </div>
+
+                        </div>
+                    </aside>
+                    <!-- /.aside -->
+                </section>
+                <a href="#" class="hide nav-off-screen-block" data-toggle="class:nav-off-screen,open" data-target="#nav,html"></a>
+            </section>
+        </section>
+    </section>
+</section>
+<script src="js/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="js/bootstrap.js"></script>
+<!-- App -->
+<script src="js/app.js"></script>
+<script src="js/slimscroll/jquery.slimscroll.min.js"></script>
+<script src="js/app.plugin.js"></script>
+<script type="text/javascript" src="js/jPlayer/jquery.jplayer.min.js"></script>
+<script type="text/javascript" src="js/jPlayer/add-on/jplayer.playlist.min.js"></script>
+<script type="text/javascript" src="js/jPlayer/demo.js"></script>
+</body>
+</html>
+
+
