783bfb7362fd876518c89cee553f776019594192
Mukuma Musenge <kuma.musenge@gmail.com>
2017-09-10 12:55:59 +0200

    Added an Event Timeline

---
+++ pages/events/events.php
@@ -0,0 +1,182 @@
+<?php
+?>
+
+<!--======= Import Custom Page CSS ====== -->
+
+ <link rel="stylesheet" href="pages/events/css/timeline.css">
+ <script src="pages/events/js/timeline.js"></script>
+
+ <!--======= End Import  ====== -->
+
+<!-- ====== Landing page content ====== -->
+
+<section id="hero-area">
+    <div class="container">
+        <div class="row">
+            <div class="">
+                <div class="block customhero">
+                    <a class="">
+                        <img src="images/about-us.png">
+                    </a>
+                </div>
+            </div>
+        </div><!-- .row close -->
+        <div>
+            <div class="block customhero">
+                <a class="">
+                    <img src="images/color_bar.png">
+                </a>
+            </div>
+        </div>
+    </div><!-- .container close -->
+</section><!-- header close -->
+
+<!--
+Timeline Section
+==================== -->
+   <section class="cd-horizontal-timeline">
+      <div class="timeline">
+        <div class="events-wrapper">
+          <div class="events">
+            <ul>
+              <li><a href="#0" data-date="16/01/2014" class="selected">16 Jan</a></li>
+              <li><a href="#0" data-date="28/02/2014">28 Feb</a></li>
+              <li><a href="#0" data-date="20/04/2014">20 Mar</a></li>
+              <li><a href="#0" data-date="20/05/2014">20 May</a></li>
+              <li><a href="#0" data-date="09/07/2014">09 Jul</a></li>
+              <li><a href="#0" data-date="30/08/2014">30 Aug</a></li>
+              <li><a href="#0" data-date="15/09/2014">15 Sep</a></li>
+              <li><a href="#0" data-date="01/11/2014">01 Nov</a></li>
+              <li><a href="#0" data-date="10/12/2014">10 Dec</a></li>
+              <li><a href="#0" data-date="19/01/2015">29 Jan</a></li>
+              <li><a href="#0" data-date="03/03/2015">3 Mar</a></li>
+            </ul>
+
+            <span class="filling-line" aria-hidden="true"></span>
+          </div> <!-- .events -->
+        </div> <!-- .events-wrapper -->
+          
+        <ul class="cd-timeline-navigation">
+          <li><a href="#0" class="prev inactive">Prev</a></li>
+          <li><a href="#0" class="next">Next</a></li>
+        </ul> <!-- .cd-timeline-navigation -->
+      </div> <!-- .timeline -->
+
+      <div class="events-content">
+        <ul>
+          <li data-date="28/02/2014">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="row">
+                        <h2>Full Name</h2>
+                        <h4>Organizational Role</h4>
+                        <em>Global Platform Zambia</em>
+                    </div>
+                    <div class="row">
+                        <blockquote>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. !</p>
+                        </blockquote>
+                    </div>
+                    <div class="row">
+                        <div class="col-sm-6 col-sm-offset-3">
+                            <a class="btn btn-social-icon btn-twitter">
+                            <span class="fa fa-twitter"></span>
+                          </a>
+                        
+                            <a class="btn btn-social-icon btn-facebook">
+                            <span class="fa fa-facebook"></span>
+                          </a>
+                        
+                            <a class="btn btn-social-icon btn-linkedin">
+                            <span class="fa fa-linkedin"></span>
+                          </a>
+                        
+                            <a class="btn btn-social-icon btn-skype">
+                            <span class="fa fa-skype"></span>
+                          </a>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <br>
+                    <br>
+                    <img src="http://via.placeholder.com/350x350" class="img-rounded" alt="Name Here">
+                </div>
+            </div>
+          </li>
+
+          <li data-date="20/04/2014">
+            <h2>Event title here</h2>
+            <em>March 20th, 2014</em>
+            <p> 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.
+            </p>
+          </li>
+
+          <li data-date="20/05/2014">
+            <h2>Event title here</h2>
+            <em>May 20th, 2014</em>
+            <p> 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.
+            </p>
+          </li>
+
+          <li data-date="09/07/2014">
+            <h2>Event title here</h2>
+            <em>July 9th, 2014</em>
+            <p> 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.
+            </p>
+          </li>
+
+          <li data-date="30/08/2014">
+            <h2>Event title here</h2>
+            <em>August 30th, 2014</em>
+            <p> 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.
+            </p>
+          </li>
+
+          <li data-date="15/09/2014">
+            <h2>Event title here</h2>
+            <em>September 15th, 2014</em>
+            <p> 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.
+            </p>
+          </li>
+
+          <li data-date="01/11/2014">
+            <h2>Event title here</h2>
+            <em>November 1st, 2014</em>
+            <p> 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.
+            </p>
+          </li>
+
+          <li data-date="10/12/2014">
+            <h2>Event title here</h2>
+            <em>December 10th, 2014</em>
+            <p> 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.
+            </p>
+          </li>
+
+          <li data-date="19/01/2015">
+            <h2>Event title here</h2>
+            <em>January 19th, 2015</em>
+            <p> 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.
+            </p>
+          </li>
+
+          <li data-date="03/03/2015">
+            <h2>Event title here</h2>
+            <em>March 3rd, 2015</em>
+            <p> 
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illum praesentium officia, fugit recusandae ipsa, quia velit nulla adipisci? Consequuntur aspernatur at, eaque hic repellendus sit dicta consequatur quae, ut harum ipsam molestias maxime non nisi reiciendis eligendi! Doloremque quia pariatur harum ea amet quibusdam quisquam, quae, temporibus dolores porro doloribus.
+            </p>
+          </li>
+        </ul>
+      </div> <!-- .events-content -->
+    </section>
+   
